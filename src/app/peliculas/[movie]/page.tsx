02583fa7204b2d7542f4e356599f959f29a0ced4
Alejandro Jerez <alejandrojerez204@gmail.com>
2024-03-29 23:54:33 -0500

    chore: SEO

--- src/app/peliculas/[movie]/page.tsx
+++ src/app/peliculas/[movie]/page.tsx
@@ -3,8 +3,23 @@ import {DetailComponent} from "app/components/detail/detail";
 import {HeaderDetail} from "app/components/detail/header";
 import Poster from "app/components/detail/poster";
 import {SoftBox} from "app/components/shared/soft_box.ts";
+import {Metadata} from "next";
 
 export const revalidate = 3600;
+
+export async function generateMetadata({searchParams}: DetailProps): Promise<Metadata> {
+    const {id} = searchParams;
+
+    const data = await getDetail(id);
+
+    return {
+        title: `Movie App | ${data.title}`,
+        description: data.overview,
+        openGraph: {
+            images: [data.backdrop_path]
+        }
+    };
+}
 
 export default async function Detail(props: DetailProps) {
     const {id} = props.searchParams;
